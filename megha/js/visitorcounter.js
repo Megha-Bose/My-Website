9a305b3beeff309256cadcbb4b114bcec66d8575
Megha Bose <56213000+Megha-Bose@users.noreply.github.com>
2020-03-09 23:32:46 +0530

    Add files via upload

---
+++ megha/js/visitorcounter.js
@@ -0,0 +1,12 @@
+var n = localStorage.getItem('on_load_counter');
+
+    if (n === null) {
+        n = 0;
+    }
+
+    n++;
+
+    localStorage.setItem("on_load_counter", n);
+
+    document.getElementById('CounterVisitor').innerHTML = n;
+
